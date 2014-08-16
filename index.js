9a5222406d9509f05f858cbeeaf9d1b03f4ec973
Ubuntu <ubuntu@ip-172-31-34-173.us-west-2.compute.internal>
2014-08-16 18:23:59 +0000

    probando leer archivos

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var express = require('express')
-aar app = express();
 
-app.set('port', (process.env.PORT || 5000))
+var express = require('express') aar app = express();
+
+app.set('port', (process.env.PORT || 5000)) 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World 2! 0')
 })
 
+app.get('/archivo', function(req, res){
+  var data = fs.readFileSync('index.html');
+  console.log(data);
+
+} 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
